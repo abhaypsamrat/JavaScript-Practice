370aac05a3488a6ecb6844d6150a25a4069882a8
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-07-19 20:15:24 +0530

    added more

--- Array/index.js
+++ Array/index.js
@@ -1,6 +1,24 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const arr1 = ["Banana", "Orange", "Apple", "Mango"];
 
-let x = fruits.toString();
-console.log(fruits)         
-console.log(typeof x);   // string
+// converting object to string
+let x = arr1.toString();
+console.log(arr1);
+console.log(typeof x); // string
 
+const arr2 = ["hello", "hi"];
+const arr3 = ["hiiii", "helllllooo"];
+
+// Merging (Concatenating) Arrays
+let arr4 = arr1.concat(arr2, arr3); // merging 2 array
+console.log(arr4); // ['Banana', 'Orange', 'Apple', 'Mango', 'hello', 'hi', 'hiiii', 'helllllooo']
+
+// JavaScript Array splice(4, 1)
+// The first parameter (4) defines the position where new elements should be added (spliced in).
+// The second parameter (1) defines how many elements should be removed.
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb"); // Inserts at index 1 replace 0 element
+console.log(months); // [("Jan", "Feb", "March", "April", "June")];
+
+months.splice(4, 1, "May"); // Inserts at index 4 replace 1 element
+console.log(months); // ['Jan', 'Feb', 'March', 'April', 'May']
