696aa89c0e48544c5abe1c284ea3c5ac9a509491
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-07-21 19:56:44 +0530

    added arrow function

---
+++ Arrow Function/index.js
@@ -0,0 +1,16 @@
+greet = function () {
+  console.log("This is Traditional function");
+};
+greet();
+
+// Arrow Function
+greet1 = () => {
+  console.log("This is Arrow function");
+};
+greet1();
+
+hello = (a, b) => {
+  return a + b + 10;
+};
+
+console.log(hello(5, 4));
