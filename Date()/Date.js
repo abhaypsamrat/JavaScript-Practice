6744fdf60bbcaa9ce979bec20fd158887e455a69
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-08-02 19:56:02 +0530

    added Date()

---
+++ Date()/Date.js
@@ -0,0 +1,10 @@
+const d = Date()
+console.log(d)    // Wed Aug 02 2023 19:48:41 GMT+0530 (India Standard Time)
+
+
+
+// Get the day of a specific date:
+
+const x = new Date("July 12, 2022 01:15:00");
+let day = x.getDate()      //The getDate() method returns the day of the month(1 to 31) of a date.
+console.log(day)  // 12
